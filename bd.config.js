7c605259393a7acb278d87a5727f789fdebfe90c
blackRYE6140 <ethanrafalimanana@gmail.com>
2024-12-30 22:51:29 +0300

    MAJ

--- bd.config.js
+++ bd.config.js
@@ -4,7 +4,10 @@ const connection = mysql.createConnection({
     host: 'bbk7ufx7dc5w3wtai7ms-mysql.services.clever-cloud.com',
     user: 'ul0korm52weekxlv',
     password: 'gDqmPonPeVSdwlfouC0V',
-    database: 'bbk7ufx7dc5w3wtai7ms'
+    database: 'bbk7ufx7dc5w3wtai7ms',
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 connection.connect((err) => {
