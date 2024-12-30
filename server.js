34c1868e832efab46a19ab94a77b97aad60c3a3e
blackRYE6140 <ethanrafalimanana@gmail.com>
2024-12-30 17:46:21 +0300

    maj2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const dashboardRoutes = require('./requete/requeteDashboard');
 const app = express();
 
 var corsOptions = {
-  origin: ['https://reservation-chambre.vercel.app/', 'http://localhost:4200'] 
+  origin: ['https://reservation-chambre.vercel.app', 'http://localhost:4200'] 
 };
 
 app.use(cors(corsOptions));
