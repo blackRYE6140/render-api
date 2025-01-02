3af52e0c917cd639e724331e9f3173d64100cd9f
blackRYE6140 <ethanrafalimanana@gmail.com>
2025-01-02 12:44:41 +0300

    atao Maj indray

--- bd.config.js
+++ bd.config.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createPool({
-    connectionLimit: 15, // Nombre maximum de connexions
+    connectionLimit: 10, // Nombre maximum de connexions
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
